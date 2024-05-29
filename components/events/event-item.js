7546ddcec74acd38e96d7921422706728f0ec403
Hyoungwoo In <io21in@hanmail.net>
2024-05-29 17:53:14 +0900

    lecture_219

--- components/events/event-item.js
+++ components/events/event-item.js
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import Image from 'next/image';
 
 import Button from '../ui/button';
 import DateIcon from '../icons/date-icon';
@@ -18,9 +18,10 @@ function EventItem(props) {
     const exploreLink = `/events/${id}`;
 
   return <li className={classes.item}>
-    <img src={'/' + image} alt={title} />
+    <Image src={'/' + image} alt={title} width={250} height={160} />
     <div className={classes.content}>
         <div className={classes.summary}>
+
             <h2>{title}</h2>
             <div className={classes.date}>
                 <DateIcon />
